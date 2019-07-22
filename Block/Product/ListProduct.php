20f12e1d3e5b4ffea474f02ff47ad66ecc2303f8
DominicWatts <dominic.watts@hotmail.co.uk>
2019-07-22 15:37:26 +0100

    Revert "Merge pull request #5 from DominicWatts/DominicWatts-patch-1"
    This reverts commit 54625650cb2c3faf96be1841d8755e8bf3e7966b.

--- Block/Product/ListProduct.php
+++ Block/Product/ListProduct.php
@@ -8,19 +8,12 @@ namespace Xigen\Special\Block\Product;
 class ListProduct extends \Magento\Catalog\Block\Product\ListProduct
 {
     /**
-     * Product collection factory
-     * @var CollectionFactory
-     */
-    protected $_productCollectionFactory;
-    
-    /**
      * ListProduct constructor.
      * @param \Magento\Catalog\Block\Product\Context $context
      * @param \Magento\Framework\Data\Helper\PostHelper $postDataHelper
      * @param \Xigen\Special\Model\Layer\Resolver $layerResolver
      * @param \Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository
      * @param \Magento\Framework\Url\Helper\Data $urlHelper
-     * @param CollectionFactory $productCollectionFactory
      * @param array $data
      */
     public function __construct(
@@ -29,10 +22,8 @@ class ListProduct extends \Magento\Catalog\Block\Product\ListProduct
         \Xigen\Special\Model\Layer\Resolver $layerResolver,
         \Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository,
         \Magento\Framework\Url\Helper\Data $urlHelper,
-        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
         array $data = []
     ) {
-        $this->_productCollectionFactory = $productCollectionFactory;
         parent::__construct(
             $context,
             $postDataHelper,
@@ -41,20 +32,5 @@ class ListProduct extends \Magento\Catalog\Block\Product\ListProduct
             $urlHelper,
             $data
         );
-    }
-    
-    /**
-     * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
-     */
-    protected function _getProductCollection()
-    {
-        if ($this->_productCollection === null) {
-            /** @var \Magento\Catalog\Model\ResourceModel\Product\Collection $collection */
-            $collection = $this->_productCollectionFactory->create();
-            $this->_catalogLayer->prepareProductCollection($collection);
-            $collection->addUrlRewrite();
-            $this->_productCollection = $collection;
-        }
-        return $this->_productCollection;
     }
 }
