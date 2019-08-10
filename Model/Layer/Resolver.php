3853dbc6572d9d15f084e10e3c1f1ec73c13ff98
Dominic <dominic.watts@hotmail.co.uk>
2019-08-10 15:39:49 +0100

    experiment with filtering

--- Model/Layer/Resolver.php
+++ Model/Layer/Resolver.php
@@ -11,6 +11,13 @@ class Resolver extends \Magento\Catalog\Model\Layer\Resolver
      * @var \Xigen\Special\Model\Layer
      */
     protected $layer;
+
+    /**
+     * Catalog view layer models list
+     *
+     * @var array
+     */
+    protected $layersPool;
     
     /**
      * Resolver constructor.
@@ -24,10 +31,18 @@ class Resolver extends \Magento\Catalog\Model\Layer\Resolver
         array $layersPool
     ) {
         $this->layer = $layer;
+        $this->layersPool = $layersPool;
         parent::__construct($objectManager, $layersPool);
     }
 
     public function create($layerType)
     {
+        if (isset($this->layer)) {
+            throw new \RuntimeException('Catalog Layer has been already created');
+        }
+        if (!isset($this->layersPool[$layerType])) {
+            throw new \InvalidArgumentException($layerType . ' does not belong to any registered layer');
+        }
+        $this->layer = $this->objectManager->create($this->layersPool[$layerType]);
     }
 }
